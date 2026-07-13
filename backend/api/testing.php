1d6e7a4206bc76dcdad8158d2f4f495d95d43159
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 23:04:07 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -1,10 +1,14 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
 header("Content-Type: application/json");
 
-
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 require __DIR__ . "/../config/database.php";
 
