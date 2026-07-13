c0145f801c03057cb9c173f9b51dcf167581613f
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 21:24:14 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-require "/../config/database.php";
+require __DIR__ . "/../config/database.php";
 
 $result = $conn->query("SELECT 1");
 
