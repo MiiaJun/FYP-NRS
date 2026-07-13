4b39d0a4b3916c83af5e9dd5fd17a3202605c545
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 20:04:00 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -4,9 +4,6 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 echo json_encode([
-    "MYSQLHOST" => getenv("MYSQLHOST"),
-    "MYSQLPORT" => getenv("MYSQLPORT"),
-    "MYSQLUSER" => getenv("MYSQLUSER"),
-    "MYSQLPASSWORD" => getenv("MYSQLPASSWORD") ? "SET" : false,
-    "MYSQLDATABASE" => getenv("MYSQLDATABASE"),
+    "mysqli_loaded" => extension_loaded("mysqli"),
+    "mysqli_exists" => class_exists("mysqli")
 ]);
