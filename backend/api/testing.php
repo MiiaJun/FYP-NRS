8f5ad172d7d4d0a644285cf3073773fb8be7dfa9
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 19:58:27 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -1,11 +1,20 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-echo json_encode([
-    "MYSQLHOST" => getenv("MYSQLHOST"),
-    "MYSQLPORT" => getenv("MYSQLPORT"),
-    "MYSQLUSER" => getenv("MYSQLUSER"),
-    "MYSQLPASSWORD" => getenv("MYSQLPASSWORD") ? "SET" : false,
-    "MYSQLDATABASE" => getenv("MYSQLDATABASE"),
-]);
+require "../config/database.php";
+
+$result = $conn->query("SELECT 1");
+
+if ($result) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Database connected!"
+    ]);
+} else {
+    echo json_encode([
+        "status" => "failed",
+        "message" => $conn->error
+    ]);
+}
