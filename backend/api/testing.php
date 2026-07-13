df99944ba191a3805aea0cc84c6421906037efbd
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 20:05:32 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -1,9 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 echo json_encode([
-    "mysqli_loaded" => extension_loaded("mysqli"),
-    "mysqli_exists" => class_exists("mysqli")
+    "pdo_mysql_loaded" => extension_loaded("pdo_mysql"),
+    "pdo_exists" => class_exists("PDO")
 ]);
