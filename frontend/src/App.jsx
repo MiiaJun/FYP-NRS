9bcc212f647971945038e1ddf3e0352dfd10049d
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 16:56:05 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("optimistic-emotion-production-0309.up.railway.app/api/tesing.php")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage("Failed to connect to PHP backend");
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>My FYP Website</h1>
-      <p>React is working!</p>
-    </div>
+    <>
+      <h1>React + PHP Test</h1>
+      <p>{message}</p>
+    </>
   );
 }
 
