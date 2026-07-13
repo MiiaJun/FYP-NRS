5bf100d3398ad5e36721a5354007c69d782b8028
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 19:02:54 +0900

    Update testing.php

--- backend/api/testing.php
+++ backend/api/testing.php
@@ -3,17 +3,4 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-require "../config/database.php";
-
-$result = $conn->query("SELECT 1");
-
-if ($result) {
-    echo json_encode([
-        "status" => "success",
-        "message" => "Database connected!"
-    ]);
-} else {
-    echo json_encode([
-        "status" => "failed"
-    ]);
-}
+echo "PHP is running";
