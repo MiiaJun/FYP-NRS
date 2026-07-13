6222886f4bdbab62846024d68aea3dafcaf9bb00
MiiaJun <dragrvejunz@gmail.com>
2026-07-13 17:21:36 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("optimistic-emotion-production-0309.up.railway.app/api/tesing.php")
+    fetch("https://optimistic-emotion-production-0309.up.railway.app/api/tesing.php")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
